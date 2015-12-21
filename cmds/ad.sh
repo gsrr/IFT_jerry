ecf8c364e2081362c68fa8ed142ecabe1bd3508a
jerry.cheng <jerry.cheng@infortrend.com>
2015-12-21 14:15:54 +0800

    update ad.sh

--- cmds/ad.sh
+++ cmds/ad.sh
@@ -4,7 +4,7 @@
 case $1 in
 	'restart')
 		echo -e "service stop ad -z A@0\nexit\n" | python /usr/local/NAS/misc/Cmd/NASCmd.py
-		echo -e "service stop ad -z A@0\nexit\n" | python /usr/local/NAS/misc/Cmd/NASCmd.py
+		echo -e "service start ad -z A@0\nexit\n" | python /usr/local/NAS/misc/Cmd/NASCmd.py
 		;;
 	*)
 		echo "no parameter"
