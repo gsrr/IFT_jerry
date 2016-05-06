9083fc3ea0e077166dbcca7ec1dadad379c77856
jerry.cheng <jerry.cheng@infortrend.com>
2016-05-06 14:58:38 +0000

    add bash prompt

---
+++ bash/clock.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function prompt_command {
+	let prompt_x=$COLUMNS-17
+}
+
+PROMPT_COMMAND=prompt_command
+
+function prompt_clock {
+local       BLUE="\[\033[0;34m\]"
+local        RED="\[\033[0;31m\]"
+local  LIGHT_RED="\[\033[1;31m\]"
+local      WHITE="\[\033[1;37m\]"
+local  NO_COLOUR="\[\033[0m\]"
+case $TERM in
+	xterm*)
+		TITLEBAR='\[\033]0;\u@\h:\w\007\]'
+		;;
+	*)
+		TITLEBAR=""
+		;;
+esac
+
+PS1="${TITLEBAR}\
+\[\033[s\033[1;\$(echo -n \${prompt_x})H\]\
+$BLUE[$LIGHT_RED\$(date +%Y%m%d-%H:%M:%S)$BLUE]\[\033[u\033[1A\]
+$BLUE[$LIGHT_RED\u@\h:\w$BLUE]\
+$WHITE\$$NO_COLOUR "
+PS2='> '
+PS4='+ '
+}
+
+function prompt_recover {
+PS1="[\u@\h:\w]\$ "
+}
