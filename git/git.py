f1b185d2c139d3e9cff46410ac4fabff9329a9a0
root <gsrr>
2016-08-25 10:21:51 +0800

    update git

--- git/git.py
+++ git/git.py
@@ -20,6 +20,22 @@ def chdir(path):
 	os.chdir(path)
 	return 0
 
+def init_bare_master(repos):
+	cmds = [
+		'echo "initial" > initial.txt',
+		'git add .',
+		'git commit -m \'initial\'',
+		'git push origin master',
+	]
+	cwd = os.getcwd()
+	chdir(cwd + "/" + repos)
+	for cmd in cmds:
+		ret = os.system(cmd)
+		if ret != 0:
+			print "(ret, cmd) = (%d, %s)"%(ret, cmd)
+	chdir(cwd)
+	return 0
+
 def init_nonbare(repos):
 	cmds = [
 		'git init',
@@ -44,7 +60,22 @@ class GIT_CREATE:
 	def __init__(self):
 		pass
 
-	def lp(self): #local Repository
+	def lp2(self):
+		repos = raw_input("Please enter the name : ")
+		cmds = [
+			'git init --bare %s.git'%repos,
+			'git clone %s.git/ %s'%(repos, repos),
+			init_bare_master,
+			'rm -rf %s'%repos,
+		]
+		for cmd in cmds:
+			if type(cmd) == str:
+				ret = os.system(cmd)
+			else:
+				ret = cmd(repos)
+			print "(ret, cmd) = (%d, %s)"%(ret, cmd)
+	
+	def lp(self): #create local Repository (bare with master branch)
 		repos = raw_input("Please enter the name : ")
 		cmds = [
 			'mkdir %s'%repos,
@@ -61,7 +92,7 @@ class GIT_CREATE:
 	def mp(self):
 		#git checkout -b $branch-name origin/$branch-name
 		branch = raw_input("Please enter the branch name : ")
-		cmd = "git checkout -b %s origin/%s"%branch
+		cmd = "git checkout -b %s origin/%s"%(branch,branch)
 		ret = os.system(cmd)
 		print "(ret, cmd) = (%d, %s)"%(ret, cmd)
 
