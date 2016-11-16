314383b8ab6c2d5158524c0a8d84ff9cf051c8a8
root <root@localhost.localdomain>
2016-11-15 22:08:59 -0500

    add tool: port_check

---
+++ tools/network/port_check.py
@@ -0,0 +1,11 @@
+import socket;
+import sys
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex((ip, port))
+if result == 0:
+    print "Port is open"
+else:
+    print "Port is not open"
