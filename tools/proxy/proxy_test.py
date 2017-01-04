936567d5d7cb4fe4f00eb0eac0389d1ca975f6c5
jerry.cheng <jerrycheng1128@gmail.com>
2017-01-05 05:38:46 +0800

    'update'

---
+++ tools/proxy/proxy_test.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+import threading
+import signal
+
+def handler(signum, frame):
+    print 'Signal handler called with signal', signum
+    raise IOError("Couldn't open device!")
+    
+
+class Proxy:
+    def __init__(self, host, dest):
+        self.left = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn = None
+        self.right = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.dest = dest
+    
+    def start(self):
+        self.left.bind((self.host, 8888))
+        self.left.listen(10)
+        self.conn, addr = self.left.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        self.right.connect((self.dest, 23))
+        
+    def handle(self):
+        send_data = self.conn.recv(1024)
+        self.right.send(send_data)
+        recv_data = self.right.recv(1024)
+        self.conn.send(recv_data)
+        
+    def close(self):
+        self.left.close()
+        self.right.close()
+        
+def main():
+    p = Proxy("localhost", "ptt.cc")
+    p.start()
+    p.handle()
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT , handler)
+    main()
