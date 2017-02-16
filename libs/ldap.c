f1dae5d89d1e826304f96ca1975cca9a4471311c
gsrr <jerrycheng1128@gmail.com>
2017-02-17 02:17:33 +0800

    update

--- libs/ldap.c
+++ libs/ldap.c
@@ -5,9 +5,9 @@
 void split2(char *name, char *s1, char *s2)
 {
     char *str1 = strtok(name,"@");
-    strncpy( str1, s1, sizeof(str1));
+    sprintf(s1, "%s", str1);
     char *str2 = strtok(NULL,"@");
-    strncpy( str2, s2, sizeof(str2));
+    sprintf(s2, "%s", str2);
 }
 
 void base2domain(char *base, char *domain)
@@ -31,23 +31,28 @@ int base_diff_domain(char *base , char *domain)
 {
     char real_domain[256] = {0};
     base2domain(base, real_domain);
-    printf("(real, domain = (%s, %s))", real_domain, domain);
     return strcmp(domain, real_domain); 
 }
 
-int name_convert(char *base, char *name, char *cn)
+int ldapname2cn(char *base, char *ldapname, char *cn)
 {
-    printf("%s, %s, %s\n", base, name, cn);
+    printf("%s, %s, %s\n", base, ldapname, cn);
     char domain[256] = {0};
-    split2(name, cn, domain);
+    split2(ldapname, cn, domain);
     printf ("%s,%s\n",cn,domain);
     if (base_diff_domain(base, domain) != 0)
     {
         printf("compare error\n");
         return -1;
     }
+    return 0;
+}
 
-    return;
+void cn2ldapname(char *base, char *cn, char *ldapname)
+{
+    char domain[256] = {0};
+    base2domain(base, domain);
+    sprintf(ldapname, "%s@%s", cn, domain);     
 }
 
 int main()
@@ -55,6 +60,12 @@ int main()
     char name[] = "test001@test.ift.com";
     char base[] = "dc=test,dc=ift,dc=com";
     char cn[256] = {0};
-    name_convert(base, name, cn);
+    ldapname2cn(base, name, cn);
+    printf("cn=%s\n",cn);
+
+    char cn2[] = "test002";
+    char name2[256] = {0};
+    cn2ldapname(base, cn2, name2);
+    printf("ldapname=%s\n", name2);
     return;
 }
