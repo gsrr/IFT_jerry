0355c42424d5e02154b8636f8685f458a6724ab2
jerry.cheng <jerrycheng1128@gmail.com>
2017-04-10 17:39:23 -0400

    update relevance feedback

--- tools/nltk/project.py
+++ tools/nltk/project.py
@@ -5,6 +5,7 @@ from nltk.stem.porter import *
 import pickle
 import os
 from numpy.linalg import norm
+import operator
 
 class Index:
     """ Inverted index datastructure """
@@ -34,11 +35,27 @@ class Index:
             word = self.stemmer.stem(word)
 
         return [self.__documents.get(id, None) for id in self.index.get(word)]
+    def add_token(self, tokens, unique_id):
+        for token in tokens:
+            if token in self.stopwords:
+                continue
+
+            if self.stemmer:
+                token = self.stemmer.stem(token)
+
+            if unique_id not in self.index[token].keys():
+                self.index[token][unique_id] = 0
+            self.index[token][unique_id] += 1
+
+            if self.documents[unique_id].has_key(token) == False:
+                self.documents[unique_id][token] = 0
+            self.documents[unique_id][token] += 1
 
     def add(self, document):
         """
             Add a document string to the index
         """
+        self.documents[self.unique_id] = {}
         for token in [t.lower() for t in nltk.word_tokenize(document)]:
             if token in self.stopwords:
                 continue
@@ -50,7 +67,11 @@ class Index:
                 self.index[token][self.unique_id] = 0
             self.index[token][self.unique_id] += 1
 
-        self.documents[self.unique_id] = document
+            if self.documents[self.unique_id].has_key(token) == False:
+                self.documents[self.unique_id][token] = 0
+            self.documents[self.unique_id][token] += 1
+
+
         print self.unique_id
         self.unique_id += 1           
 
@@ -64,28 +85,110 @@ def create_corpus():
             data = fr.read()
             index.add(data)
 
-    pickle.dump( index.index, open( "./inverted_index_file", "wb" ) )
+    pickle.dump( index, open( "./inverted_index_file", "wb" ) )
 
 def usage():
     print "1 : tf with cosine"
     print "2 : tf with Jaccard"
     print "3 : tf-idf with cosine"
     print "4 : tf-idf with Jaccard"
+    print "5 : tf-idf with Jaccard(Relevance Feedback)"
 
-def cosine(q, c):
-    q_vec = [q[word][0] for word in q.keys()]
-    print q_vec
+def tf_cosine(q, c):
     ret = {}
     s = 0
-    for word in q.keys():
-        for d in c[word]:
+    for word in q.index.keys():
+        for d,val in c.index[word].items():
             if ret.has_key(d) == False:
                 ret[d] = 0
-            ret[d] += (q[word][0] * c[word][d])
-        
+            ret[d] += (q.index[word][0] * val)
+
+    q_vec = [val for word,val in q.documents[0].items()]
+    for d in ret.keys():
+        d_vec = [val for word,val in c.documents[d].items()]
+        #print d, ret[d], q_vec, d_vec
+        ret[d] = ret[d]/(norm(q_vec)*norm(d_vec))
+    return ret
+
+def tf_jaccard(q, c):
+    ret = {}
+    s = 0
+    for word in q.index.keys():
+        for d,val in c.index[word].items():
+            if ret.has_key(d) == False:
+                ret[d] = 0
+            ret[d] += 1     # intersection
+
+    q_set = len(q.documents[0].keys())
+    for d in ret.keys():
+        d_set = len(c.documents[d].keys())
+        #print d, ret[d], q_set, d_set
+        ret[d] = float(ret[d]) / (q_set + d_set - ret[d])
+    return ret
+
+def tfidf_cosine(q, c):
+    ret = {}
+    s = 0
+    for word in q.index.keys():
+        for d,val in c.index[word].items():
+            if ret.has_key(d) == False:
+                ret[d] = 0
+            val = float(val) / len(c.index[word].keys())   # tf / idf
+            ret[d] += (q.index[word][0] * val)
+
+    q_vec = [val for word,val in q.documents[0].items()]
+    for d in ret.keys():
+        d_vec = [float(val) / len(c.index[word].keys()) for word,val in c.documents[d].items()]
+        #print d, ret[d], q_vec, d_vec
+        ret[d] = ret[d]/(norm(q_vec)*norm(d_vec))
+    return ret
+
+def tfidf_jaccard(q, c):
+    ret = {}
+    s = 0
+    for word in q.index.keys():
+        for d,val in c.index[word].items():
+            if ret.has_key(d) == False:
+                ret[d] = 0
+            ret[d] += 1     # intersection
+
+    q_set = len(q.documents[0].keys())
+    for d in ret.keys():
+        d_set = len(c.documents[d].keys())
+        #print d, ret[d], q_set, d_set
+        ret[d] = float(ret[d]) / (q_set + d_set - ret[d])
+    return ret
+
+def output(ret, doc_map):
+    sr = sorted(ret.items(), key=operator.itemgetter(1))
+    print "\tDocId\tScore"
+    for item in sr[::-1][:10]:
+        print "\t%s\t%f"%(doc_map[str(item[0])], item[1])
+
+def get_relev_str(doc_path):
+    ret = []
+    fr = open(doc_path, "r")
+    doc = fr.read()
+    tokens = nltk.word_tokenize(doc)
+    pos_tokens = nltk.pos_tag(tokens)
+    for word in pos_tokens:
+        if word[1] == "NN" or word[1] == "VB":
+            ret.append(word[0])
+    return ret
+
+def get_doc_map():
+    dic = {}
+    with open("map_documents", "r") as fr:
+        lines = fr.readlines()
+        for line in lines:
+            line = line.strip()
+            key,value = line.split(None, 1)
+            dic[value] = key
+    return dic
 
 def main():
-    inverted_index = pickle.load(open( "inverted_index_file", "rb" ))
+    doc_map = get_doc_map()
+    index = pickle.load(open( "inverted_index_file", "rb" ))
     while True:
         usage()
         opt = raw_input("Enter your option:")
@@ -93,10 +196,26 @@ def main():
         print "opt=%s"%opt, "query=%s"%query
         index_query = Index(nltk.word_tokenize, EnglishStemmer(), nltk.corpus.stopwords.words('english'))
         index_query.add(query)
-        cosine(index_query.index, inverted_index)
+        if opt == "1" :
+            ret = tf_cosine(index_query, index)
+        elif opt == "2" :
+            ret = tf_jaccard(index_query, index)
+        elif opt == "3":
+            ret = tfidf_cosine(index_query, index)
+        elif opt == "4":
+            ret = tfidf_jaccard(index_query, index)
+        elif opt == "5":
+            while True:
+                ret = tfidf_jaccard(index_query, index)
+                output(ret, doc_map)
+                doc_path = raw_input("Please Enter doc path for relevance feedback:")
+                relev_toks = get_relev_str(doc_path)
+                index_query.add_token(relev_toks, 0)
+        else:
+            pass
 
-
-
+        output(ret, doc_map)
 if __name__ == "__main__":
     #create_corpus()
     main()
+    #print get_relev_str("documents/159196.product")
